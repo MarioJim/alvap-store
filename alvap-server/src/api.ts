5e5e016960da6ffdf3b180c2043e4b9d0eab28dc
MarioJim <mario.emilio.j@gmail.com>
2020-06-01 23:07:13 -0500

    Added sqlite database

--- alvap-server/src/api.ts
+++ alvap-server/src/api.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import { dbConnection } from './database';
+
+// From https://developerhowto.com/2018/12/29/build-a-rest-api-with-node-js-and-express-js/
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('GET to /api/')
+  const sql = 'SELECT sqlite_version();'
+  dbConnection.get(sql, (err, row) => {
+    if (err) {
+      res
+        .status(400)
+        .json({ error: err.message, });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row,
+    });
+  });
 });
 
 // TODO: Append api methods
---
+++ alvap-server/src/database.ts
@@ -0,0 +1,15 @@
+import sqlite3 from 'sqlite3';
+import path from 'path';
+
+sqlite3.verbose();
+
+const DB_PATH = path.join(__dirname, '..', 'alvap.db');
+
+export const dbConnection = new sqlite3.Database(DB_PATH, (err) => {
+  if (err) {
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log('Connected to database');
+  }
+});
