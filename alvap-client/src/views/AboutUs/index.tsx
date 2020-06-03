d02f63e58d1b2f2343804fb6ad700efe424a6cc4
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-06-02 22:11:43 -0500

    About us page

--- alvap-client/src/views/AboutUs/index.tsx
+++ alvap-client/src/views/AboutUs/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import facebook from "./facebook.png"
+import twitter from "./twitter.png"
+import instagram from "./instagram.png"
+import { Link } from 'react-router-dom';
 
 const AboutUs: React.FunctionComponent = () => (
   <div>
     <h1>About Us page</h1>
+    {/*<Link to="www.facebook.com"></Link>*/}
+    <img src={facebook} alt="Facebook logo" className="logo" id="facebook"/>
+    <img src={twitter} alt="Twitter logo" className="logo" id="twitter" />
+    <img src={instagram} alt="instagram logo" className="logo" id="instagram" />
+    <p>Remember to follow us on our social media to know more about us and get notified about promotios and deals</p>
   </div>
 );
 
