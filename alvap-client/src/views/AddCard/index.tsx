afe25688ef8edb8eff633f19110ccfaa5f7d304f
Lars17S <efren.garibaldi@gmail.com>
2020-06-07 15:00:23 -0500

    Update AddCard.tsx

--- alvap-client/src/views/AddCard/index.tsx
+++ alvap-client/src/views/AddCard/index.tsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import {
+  Form, 
+  Grid,
+  Header,
+  Segment,
+  FormInput,
+  Table,
+  Message
+} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const AddCard: React.FunctionComponent = () => (
-  <div>
-    <h1>Add Card page</h1>
-  </div>
+  <Grid textAlign="center" style={{ height: '100vh'}} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450}}>
+      <Header as="h2" color="teal" textAlign="center">
+        Ingresa los datos de tu tarjeta
+      </Header>
+      <Form size="large">
+        <Segment stacked>
+          <Form.Input
+            fluid
+            icon="credit card"
+            iconPosition="left"
+            placeholder="Numero de tarjeta"
+          />
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="Nombre del titular"
+          />
+          <Table celled>
+            <Table.Row>
+              <Table.Cell>
+                <Form.Input
+                  fluid
+                  icon="calendar"
+                  iconPosition="left"
+                  placeholder="month"
+                />
+              </Table.Cell>
+              <Table.Cell>
+                <Form.Input
+                  fluid
+                  icon="calendar"
+                  iconPosition="left"
+                  placeholder="year"
+                />
+              </Table.Cell>
+            </Table.Row>
+          </Table>
+          <FormInput
+            fluid 
+            icon="credit card outline"
+            iconPosition="left"
+            placeholder="cvv"
+          />
+        </Segment>
+      </Form>
+      <Message>
+        <Link to="/confirmacion">Confirmar método de pago</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
 );
 
 export default AddCard;
