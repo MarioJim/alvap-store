5fc69f9d4aa79abb32ad43a9dec993b8effc8ade
MarioJim <mario.emilio.j@gmail.com>
2020-06-08 17:38:56 -0500

    Connected Product to API

--- alvap-client/src/views/Product/index.tsx
+++ alvap-client/src/views/Product/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { getFromApi } from '../../utils';
 
@@ -9,12 +9,23 @@ interface MatchParams {
 interface Props extends RouteComponentProps<MatchParams> {}
 
 const Product: React.FunctionComponent<Props> = ({ match }) => {
+  const [product, setProduct] = useState<any>({ id: -1 });
   useEffect(() => {
-    getFromApi(`/products/${match.params.id}`).then((res) => console.log(res));
-  });
+    getFromApi(`/products/${match.params.id}`).then((res) => {
+      console.log(res);
+      setProduct(res);
+    });
+  }, [match.params.id]);
   return (
     <div>
       <h1>Product page for product {match.params.id}</h1>
+      {product.id === -1 ? (
+        <div>Loading</div>
+      ) : (
+        <div>
+          <p>{product.descripcion}</p>
+        </div>
+      )}
     </div>
   );
 };
