de932344c8d933b479b592c1dccb05f69f7df910
Lars17S <efren.garibaldi@gmail.com>
2020-06-08 14:50:19 -0500

    order-page to shopping-cart page

--- alvap-client/src/views/ShoppingCart/index.tsx
+++ alvap-client/src/views/ShoppingCart/index.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { RouteComponentProps } from 'react-router-dom'
+import { getFromApi } from '../../utils';
+import Product from '../Store/product';
 
-const ShoppingCart: React.FunctionComponent = () => (
-  <div>
-    <h1>Shopping Cart page</h1>
-  </div>
-);
+interface MatchParams {
+  id: string;
+}
+
+interface Props extends RouteComponentProps<MatchParams> {}
+
+const ShoppingCart: React.FunctionComponent<Props> = ({ match }) => {
+  const [order, setOrder] = useState<any[]>([]);
+  useEffect(() => {
+    getFromApi(`/carts/${match.params.id}`).then((res) => {
+      console.log(res);
+      setOrder(res);
+    });
+  }, [match.params.id]);
+  return (
+    <div>
+      <h1>ShoppingCart page for order {match.params.id}</h1>
+      {order.length === 0 ? (
+        <div>Carrito vacío</div>
+      ) : (
+        order.map((producto) => (
+          <Product
+            key={producto.id}
+            id={producto.id}
+            nombre={producto.nombre}
+            precio={producto.precio}
+          />
+        ))
+      )}
+    </div>
+  );
+};
 
 export default ShoppingCart;
