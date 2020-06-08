78bb2aac22f64f9c05df5d0756853a9ab7715139
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-06-08 14:01:48 -0500

    Update index.tsx

--- alvap-client/src/views/Coupons/index.tsx
+++ alvap-client/src/views/Coupons/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { getFromApi } from '../../utils';
 
 const Coupons: React.FunctionComponent = () => (
-  
+  <div></div>
 );
 
 export default Coupons;
