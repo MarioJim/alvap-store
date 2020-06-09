b9d8311871463b2b9d0b5a49f89809ba492362b2
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-06-08 19:17:03 -0500

    Product page design

--- alvap-client/src/components/Nav/index.tsx
+++ alvap-client/src/components/Nav/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Burger from './Burger';
+import { Link } from 'react-router-dom';
 
 const Nav = styled.nav({
   width: '100%',
-  height: '55px',
+  height: 'auto',
   borderBottom: '2px solid #f1f1f1',
   padding: '0 20px',
   display: 'flex',
   justifyContent: 'space-between',
+  zIndex:10,
 });
 
 const Name = styled.div({
@@ -17,7 +19,7 @@ const Name = styled.div({
 
 const Navbar: React.FunctionComponent = () => (
   <Nav>
-    <Name className="title">AL-VAP</Name>
+    <Link to="/tienda" style={{color:'#111', fontSize: '2.5rem'}}><Name className="title">AL-VAP</Name></Link>
     <Burger />
   </Nav>
 );
--- alvap-client/src/views/Product/index.tsx
+++ alvap-client/src/views/Product/index.tsx
@@ -1,12 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Link } from 'react-router-dom';
 import { getFromApi } from '../../utils';
+import styled from '@emotion/styled';
+import unavailable from '../Store/unavailable.png'
 
 interface MatchParams {
   id: string;
 }
 
 interface Props extends RouteComponentProps<MatchParams> {}
+
+const StyleProduct=styled.div<MatchParams>`
+  h1, h2, p{
+    text-align:center;
+    margin:0 5%;
+  }
+  h1{
+    margin:5%;
+    font-size: 2.25rem;
+    background-color: #e6fffe;
+    padding: 1.25% 0;
+  }
+  p{
+    margin:0 5% 5% 5%;
+    font-size: 1.51428571rem;
+    border: 1px solid #333;
+    padding: 2.5% 0;
+  }
+  img{
+    width: 75%;
+    margin:2.5% 12.5%;
+  }
+  button{
+    width: 75%;
+    margin: 0 12.5% 5% 12.5%;
+    font-size: 1.51428571rem;
+    padding: 2.5% 0;
+    border:1px solid #bbb;
+  }
+
+  .carrito{
+    background-color:#ccfffd;
+    border:none;
+  }
+
+  a{
+    color: black;
+  }
+
+  .regresar{
+    background-color:#fff;
+  }
+`;
 
 const Product: React.FunctionComponent<Props> = ({ match }) => {
   const [product, setProduct] = useState<any>({ id: -1 });
@@ -17,16 +62,20 @@ const Product: React.FunctionComponent<Props> = ({ match }) => {
     });
   }, [match.params.id]);
   return (
-    <div>
-      <h1>Product page for product {match.params.id}</h1>
+    <StyleProduct id={match.params.id}>
       {product.id === -1 ? (
         <div>Loading</div>
       ) : (
         <div>
-          <p>{product.descripcion}</p>
+            <h1>{product.nombre}</h1>
+            <h2>${product.precio}.00</h2>
+            {product.foto === null ? (<img src={unavailable} alt="Not available" />) : (<img src={product.foto} alt={product.nombre + " foto"} />)}
+            <p>{product.descripcion}</p>
+            <button className="carrito">Añadir al carrito</button>
+            <Link to="/tienda"><button className="regresar">Regresar</button></Link>
         </div>
       )}
-    </div>
+    </StyleProduct>
   );
 };
 
