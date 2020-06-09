3140373f3cf189c49e45b409ca15c7af436cb7ec
Lars17S <efren.garibaldi@gmail.com>
2020-06-08 21:52:38 -0500

    AddCard.tsx updated

--- alvap-client/src/views/AddCard/index.tsx
+++ alvap-client/src/views/AddCard/index.tsx
@@ -1,68 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Form, 
   Grid,
   Header,
   Segment,
   FormInput,
-  Table,
-  Message
+  Message,
+  Button
 } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { postToApi } from '../../utils';
+import { useHistory } from 'react-router-dom';
 
-const AddCard: React.FunctionComponent = () => (
-  <Grid textAlign="center" style={{ height: '100vh'}} verticalAlign="middle">
-    <Grid.Column style={{ maxWidth: 450}}>
-      <Header as="h2" color="teal" textAlign="center">
-        Ingresa los datos de tu tarjeta
-      </Header>
-      <Form size="large">
-        <Segment stacked>
-          <Form.Input
-            fluid
-            icon="credit card"
-            iconPosition="left"
-            placeholder="Numero de tarjeta"
+const AddCard: React.FunctionComponent = () => {
+  const history = useHistory();
+  const [num, setNum] = useState('');
+  const [titular, setTitular] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [cvv, setCvv] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const handleSubmit = () => {
+    postToApi('/cards', {
+      titular,
+      num,
+      fecha,
+      cvv,
+    }).then((res) => {
+      if (res.error) {
+        setError(res.error[0]);
+      } else {
+        setError('');
+        setSuccess(true);
+        setTimeout(() => {
+          history.push('/tienda');
+        }, 2000);
+      }
+    })
+  };
+  return (
+    <Grid textAlign="center" style={{ height: '100vh'}} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450}}>
+        <Header as="h2" color="teal" textAlign="center">
+          Ingresa los datos de tu tarjeta
+        </Header>
+        {success && (
+          <Message
+            success
+            header="Listo"
+            content={`Tu tarjeta se ha añadido`}
           />
-          <Form.Input
-            fluid
-            icon="user"
-            iconPosition="left"
-            placeholder="Nombre del titular"
-          />
-          <Table celled>
-            <Table.Row>
-              <Table.Cell>
-                <Form.Input
-                  fluid
-                  icon="calendar"
-                  iconPosition="left"
-                  placeholder="month"
-                />
-              </Table.Cell>
-              <Table.Cell>
-                <Form.Input
-                  fluid
-                  icon="calendar"
-                  iconPosition="left"
-                  placeholder="year"
-                />
-              </Table.Cell>
-            </Table.Row>
-          </Table>
-          <FormInput
-            fluid 
-            icon="credit card outline"
-            iconPosition="left"
-            placeholder="cvv"
-          />
-        </Segment>
-      </Form>
-      <Message>
-        <Link to="/confirmacion">Confirmar método de pago</Link>
-      </Message>
-    </Grid.Column>
-  </Grid>
-);
+        )}
+        {error !== '' && <Message error header="Error" content={error} />}
+        <Form size="large" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="credit card"
+              iconPosition="left"
+              placeholder="Numero de tarjeta"
+              autoComplete="numTarjeta"
+              value={num}
+              onChange={(_, { value }) => setNum(value)}
+            />
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="Nombre del titular"
+              autoComplete="nombreTitular"
+              value={titular}
+              onChange={(_, { value }) => setTitular(value)}
+            />
+            <Form.Input
+              fluid
+              icon="calendar"
+              iconPosition="left"
+              placeholder="Fecha (MM/YY)"
+              autoComplete="XX/XX"
+              value={fecha}
+              onChange={(_, { value }) => setFecha(value)}
+            />
+            <FormInput
+              fluid 
+              icon="credit card outline"
+              iconPosition="left"
+              placeholder="cvv"
+              autoComplete="XXX"
+              value={cvv}
+              onChange={(_, { value }) => setCvv(value)}
+            />
+            <Button color="teal" fluid size="large" type="submit">
+              Añadir tarjeta
+            </Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  );
+};
 
 export default AddCard;
